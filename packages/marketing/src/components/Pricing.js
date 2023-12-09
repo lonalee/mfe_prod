977ced9c9e67e1084699f93a88a302d579f255bb
lonalee <42775674+lonalee@users.noreply.github.com>
2023-12-09 14:27:00 +0900

    edit marketing re-deploy

--- packages/marketing/src/components/Pricing.js
+++ packages/marketing/src/components/Pricing.js
@@ -153,7 +153,7 @@ export default function Pricing() {
           color="textPrimary"
           gutterBottom
         >
-          Pricing
+          Pricing 0
         </Typography>
         <Typography
           variant="h5"
