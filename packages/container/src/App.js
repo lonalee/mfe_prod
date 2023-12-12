c03bebbf1aece026b9cdd7acb667153c7fda9d7b
lonalee2018 <lonalee2018@gmail.com>
2023-12-12 22:55:34 +0900

    css solution
    use createGenerateClassName

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,27 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 
 export default () => {
+  const generateClassName = createGenerateClassName({
+    productionPrefix: "co",
+  });
 
-    // 그냥 리액트 컴포넌트를 export/import하면 안 되나? mount를 import하는 이유..
-    // container & child Apps 사이에 커플링을 없애기 위해서..
-    // 심지어 양쪽 어디에서든 기술적으로 변화를 가해도 서로 영향을 주고 받지 않기 위해서...
-    return <BrowserRouter>
+  // 그냥 리액트 컴포넌트를 export/import하면 안 되나? mount를 import하는 이유..
+  // container & child Apps 사이에 커플링을 없애기 위해서..
+  // 심지어 양쪽 어디에서든 기술적으로 변화를 가해도 서로 영향을 주고 받지 않기 위해서...
+  return (
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
         <Header />
         <MarketingApp />
-        </BrowserRouter>
-}
+      </StylesProvider>
+    </BrowserRouter>
+  );
+};
