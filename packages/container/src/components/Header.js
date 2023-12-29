29f6c78afc5db69eff0a596fc327ef3d1593164e
lonalee <42775674+lonalee@users.noreply.github.com>
2023-12-29 22:42:19 +0900

    rename title

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -79,7 +79,7 @@ export default function Header({ signedIn, onSignOut }) {
             component={RouterLink}
             to="/"
           >
-            App
+            My Place
           </Typography>
           <Button
             color="primary"
