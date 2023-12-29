e1ea048144e959b9494066861f22606869760f9d
lonalee <42775674+lonalee@users.noreply.github.com>
2023-12-29 21:47:11 +0900

    wire up Auth
    - route to Auth & Marketing
    - setup webpack dev config

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
 } from "@material-ui/core/styles";
 
+import AuthApp from "./components/AuthApp";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 
@@ -20,6 +21,10 @@ export default () => {
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
         <Header />
+        <Switch>
+          <Route path="/auth" component={AuthApp} />
+          <Route path="/" component={MarketingApp} />
+        </Switch>
         <MarketingApp />
       </StylesProvider>
     </BrowserRouter>
